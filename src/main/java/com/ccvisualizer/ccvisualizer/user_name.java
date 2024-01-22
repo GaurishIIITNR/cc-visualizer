d5a1e5d37664fb9f165f509236cb35d9d25c2f19
Gaurish Ojha <97218624+GaurishIIITNR@users.noreply.github.com>
2024-01-22 10:24:30 +0530

    Update user_name.java

--- src/main/java/com/ccvisualizer/ccvisualizer/user_name.java
+++ src/main/java/com/ccvisualizer/ccvisualizer/user_name.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 public class user_name {
 
     // replace your username
-    private String username = "shibu_10";
+    private String username = "atal21003";
 
     // if getting out of bound issue please recheck and verify your username
 }
